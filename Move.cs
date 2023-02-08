7aeb5a83edd4e023f54152f4a6c71386b3edf3d1
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2023-02-08 09:40:40 -0500

    Update Move.cs

--- Move.cs
+++ Move.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    private float MouseX = 0f;
-    private float MouseY = 0f;
     public float Sensitivity = 2f;
+    public float JP = 2f;
+    public Rigidbody rb;
 
 
-    void Update()
-    {
-
-        if (Input.GetMouseButton(0))
-        {
-            MouseX += Sensitivity * Input.GetAxis("Mouse X");
-            MouseY -= Sensitivity * Input.GetAxis("Mouse Y");
-        }
-        transform.eulerAngles = new Vector3(MouseY, MouseX, 0f);
+    void Update() { 
         transform.position += (transform.forward * Input.GetAxisRaw("Vertical") * 0.1f);
         transform.position += (transform.right * Input.GetAxisRaw("Horizontal") * 0.1f);
-        if(Input.GetKey("space")){
-            transform.position += new Vector3(0,0.1f,0);
+        if (Input.GetKeyDown("space")){
+            rb.AddForce(0,JP,0);
         }
     }
            
